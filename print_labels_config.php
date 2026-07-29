a6590dc95b0d552cf2007a3106a7778c6644365e
dylan.weber <dylan.weber@southlandsteel.com>
2026-07-29 10:10:11 -0500

    Add Amite Office label printer

--- print_labels_config.php
+++ print_labels_config.php
@@ -6,5 +6,10 @@ return [
             'label' => 'IT Office',
             'ip'    => '192.168.80.44',  // TODO: set actual label printer IP
         ],
+
+        'amite_manager' => [
+            'label' => 'Amite Office',
+            'ip'    => '192.168.4.170',  // TODO: set actual label printer IP
+        ],
     ],
 ];
